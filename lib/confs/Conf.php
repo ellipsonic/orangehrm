59c69e47bc899701c582cd20617ad4abc0cac100
root <root@server.ellipsonic>
2015-07-21 00:29:55 -0400

    Push from linux

---
+++ lib/confs/Conf.php
@@ -0,0 +1,28 @@
+<?php
+class Conf {
+
+	var $smtphost;
+	var $dbhost;
+	var $dbport;
+	var $dbname;
+	var $dbuser;
+	var $version;
+
+	function Conf() {
+
+		$this->dbhost	= '84.200.193.32';
+		$this->dbport 	= '3306';
+		if(defined('ENVIRNOMENT') && ENVIRNOMENT == 'test'){
+		$this->dbname    = 'test_orangehrm2';		
+		}else {
+		$this->dbname    = 'orangehrm2';
+		}
+		$this->dbuser    = 'orangehrm';
+		$this->dbpass	= '';
+		$this->version = '3.3.1';
+
+		$this->emailConfiguration = dirname(__FILE__).'/mailConf.php';
+		$this->errorLog =  realpath(dirname(__FILE__).'/../logs/').'/';
+	}
+}
+?>
