17f0084585c0bc1e33e1305df8c461ab557824c7
root <root@server.ellipsonic>
2015-07-24 02:57:06 -0400

    Changes

--- lib/confs/Conf.php
+++ lib/confs/Conf.php
@@ -13,12 +13,12 @@ class Conf {
 		$this->dbhost	= '84.200.193.32';
 		$this->dbport 	= '3306';
 		if(defined('ENVIRNOMENT') && ENVIRNOMENT == 'test'){
-		$this->dbname    = 'test_orangehrm2';		
+		$this->dbname    = 'test_orangehrm_mysql_d';		
 		}else {
-		$this->dbname    = 'orangehrm2';
+		$this->dbname    = 'orangehrm_mysql_d';
 		}
-		$this->dbuser    = 'orangehrm';
-		$this->dbpass	= '';
+		$this->dbuser    = 'root';
+		$this->dbpass	= 'password';
 		$this->version = '3.3.1';
 
 		$this->emailConfiguration = dirname(__FILE__).'/mailConf.php';
